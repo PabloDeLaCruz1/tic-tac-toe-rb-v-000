720c38721efb41d1a58c1b5a3167d517fbc6add5
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-26 00:49:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,8 @@
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], 
   [3,4,5],
@@ -10,16 +14,54 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-# def turn_count(board)
-#   count = 0
-#   board.each do |index|
-#     if index == "O" || index == "X"
-#       count += 1
-#     end
-#   end 
-#   return count
-# end
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-# def current_player(board)
-#   turn_count(board) % 2 == 0 ? "X" : "O"
-# end 
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || 
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
+       return win_combination
+    end
+  end
+  false
+end
+
+def full?(board)
+ board.include?(" ") ? false : true
+end 
+
+def draw?(board)
+  won?(board) || !full?(board) ? false : true
+end
+
+def over?(board)
+  draw?(board) || full?(board) || won?(board) ? true : false
+end
+
+def winner(board)
+  if(won?(board))
+    board[won?(board)[0]]
+  else
+    nil
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index == "O" || index == "X"
+      count += 1
+    end
+  end 
+  return count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end 
