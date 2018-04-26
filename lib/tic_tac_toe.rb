329396fabab8b2d12d0f065775a9d94c21475635
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-26 00:52:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,6 +22,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+ def input_to_index(user_input)
+   user_input.to_i - 1
+ end
+
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
